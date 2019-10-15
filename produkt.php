04a2ed0ba4620f163e4ac99e93941f096549d86f
Artur Dymkowski <arturdymkowski@wp.pl>
2019-10-15 11:59:18 +0200

    Poprawa produktow

--- produkt.php
+++ produkt.php
@@ -6,12 +6,18 @@
 
         $zapytanie = $polaczenie->query("SELECT * FROM produkty WHERE id=$id");
         while($row = $zapytanie->fetch_array()) {
+            echo "<div class=container>";
+            echo "<div class=row>";
+            echo "<div class=col-lg-12>";
             foreach (pobierzZdjecia($row['index']) as $image) {
                 echo "<img class='img-fluid' src='img/$image'>";
             }
             echo '<h2 class="h1">'.$row['nazwa'].'</h2>';
             echo '<h4>'.$row['cena'].' zł</h4>';
             echo '<p>'.$row['opis'].'</p>';
+            echo "</div>";
+            echo "</div>";
+            echo "</div>";
         }
     }
 
